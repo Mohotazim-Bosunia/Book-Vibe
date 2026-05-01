e3ead3f03fcaa64fff2e53268ff83eb3a82604d0
Mohotazim Hossen Bosunia <zimhossen0@gmail.com>
2026-05-01 23:55:00 +0600

    fifth commit

--- src/Pages/Book/Book.jsx
+++ src/Pages/Book/Book.jsx
@@ -1,26 +1,31 @@
-import React from 'react';
+import React from "react";
+import { FaRegStar } from "react-icons/fa";
 
-const Book = ({book}) => {
-    const {bookName,image} = book;
-    return (
-        <div>
-         <div className="card bg-base-100 w-96 shadow-sm border">
-  <figure>
-    <img
-    className=' h-40  mx-auto'
-      src={image}
-      alt='bookImg' />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">Card Title</h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
+const Book = ({ book }) => {
+  const { bookName, image, author,category,rating } = book;
+  return (
+    <div>
+      <div className="card bg-base-100 w-96 shadow-sm border p-4 rounded-lg">
+        <figure className="bg-gray-200 w-2/3 mx-auto p-4 rounded-xl mt-3">
+          <img className=" h-40 mx-auto " src={image} alt="bookImg" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title text-2xl font-semibold">{bookName}</h2>
+          <p className="font-semibold">By:
+           {author}
+          </p>
+          <hr class="border-0 border-t-2 border-dashed border-gray-300 m-3"></hr>
+          <div className="flex justify-between items-center">
+            <span>{category}</span>
+            <div className="flex justify-between gap-2">
+              <span className="font-semibold">{rating} </span>
+              <span className="mt-1"><FaRegStar /></span>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Book;
--- src/Pages/Books/Books.jsx
+++ src/Pages/Books/Books.jsx
@@ -3,7 +3,7 @@ import Book from '../Book/Book';
 
 const Books = ({booksData}) => {
     return (
-        <div className='grid md:grid-cols-3 gap-10 mt-10'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  gap-15 mt-10'>
            {booksData.map(book => 
            <Book 
            book={book}
